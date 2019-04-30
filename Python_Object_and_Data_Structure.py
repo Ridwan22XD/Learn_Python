4eec10f414e1d7a9f7af5c7daa4e4c063e4b62c5
Ridwan22XD <mre22xd@gmail.com>
2019-04-30 23:57:23 +0700

    Mengenal Python Number

---
+++ Python_Object_and_Data_Structure.py
@@ -0,0 +1,19 @@
+
+                                #Python Numbers
+a=10
+b=5
+
+#modulo
+c=a%b;
+
+#pangkat
+d=a**b;
+
+#tambah,kurang, bagi, kali
+e=1*3+2/2;
+f=1*(3+2)/2;
+print(a," modulo ", b, ":", c);
+print(a," pangkat ", b, ":", d);
+print("1*3+2/2: ",e);
+print("1*(3+2)/2: ",f);
+
