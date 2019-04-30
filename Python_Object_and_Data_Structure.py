4623c8bd721e8cdef4866a5d94d74694c1917690
Ridwan22XD <mre22xd@gmail.com>
2019-05-01 00:42:28 +0700

    Pengenalan String

--- Python_Object_and_Data_Structure.py
+++ Python_Object_and_Data_Structure.py
@@ -1,5 +1,6 @@
 
                                 #Python Numbers
+print("\t\t\t\tPython Number");
 a=10
 b=5
 
@@ -17,3 +18,18 @@ print(a," pangkat ", b, ":", d);
 print("1*3+2/2: ",e);
 print("1*(3+2)/2: ",f);
 
+
+                                #Introduction String
+print("\t\t\t\tIntroduction String");
+a = 'hello world'
+b = 'hello \'world'
+c = "hello world"
+d = "hello 'world'"
+e = "hello \"world\""
+
+print("menggunakan titik satu: ",a);
+print("menggunakan titik satu dan satu: ",b);
+print("menggunakan titik satu dua: ",c);
+print("menggunakan titik satu dua dan satu: ",d);
+print("menggunakan titik satu dua dan dua: ",e);
+print("panjang string a: ",len(a));
